03b94941b6d18c5a738c9df14905b9ea089d0200
calvinrahmat <calvin.rahmat@gmail.com>
2021-07-04 10:56:24 +0700

    add filter by id

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ctrlProducts = require('../controllers/products');
 
-router.get('/:category', ctrlProducts.getAll);
+router.get('/', ctrlProducts.getAll);
 router.post('/addData', ctrlProducts.addData);
-router.get('/category/:category', ctrlProducts.getCategory);
+router.get('/kategori/:id', ctrlProducts.getCategory);
 
 module.exports = router;
